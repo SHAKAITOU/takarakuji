0a96ccdca9343949d90ee51251f1efa004c25d6c
SHAKAITOU <h-xie@ik.cnt.jp.nec.com>
2017-10-24 16:05:34 +0900

    test

---
+++ src/main/java/sha/framework/TakarakujiApplication.java
@@ -0,0 +1,12 @@
+package sha.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakarakujiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakarakujiApplication.class, args);
+	}
+}
---
+++ src/test/java/sha/framework/TakarakujiApplicationTests.java
@@ -0,0 +1,16 @@
+package sha.framework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TakarakujiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
